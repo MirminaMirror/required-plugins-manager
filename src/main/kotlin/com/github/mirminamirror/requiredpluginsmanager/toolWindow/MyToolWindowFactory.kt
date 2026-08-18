541beea7b754cd16c7a427f274989bd22d7d7224
GitHub Action <action@github.com>
2026-08-18 15:42:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/mirminamirror/requiredpluginsmanager/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mirminamirror/requiredpluginsmanager/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mirminamirror.requiredpluginsmanager.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mirminamirror.requiredpluginsmanager.MyBundle
+import com.github.mirminamirror.requiredpluginsmanager.services.MyProjectService
 import javax.swing.JButton
 
 
